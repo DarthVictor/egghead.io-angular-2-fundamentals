2d5d80e894152ff105b4695da660d8f8d6d15832
Victor Follet <follet.victor@gmail.com>
2016-03-11 00:45:07 +0300

    Third and fourth videos

---
+++ 03-events-and-refs/todo-input.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'todo-input',
+	template: `<div>
+	<input type="text" #myInput>
+	<button (click)="onClick(myInput)">Click me</button>
+	</div>`
+})
+export class TodoInput {
+	
+	constructor() {
+	}
+
+	onClick(value){
+		console.log(value);
+	}
+}
