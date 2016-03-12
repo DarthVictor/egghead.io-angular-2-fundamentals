fe188e19de46b219ba252dc07869bcd2277a2e74
Victor Follet <follet.victor@gmail.com>
2016-03-12 17:37:00 +0300

    Video 8 + list of names for folders

---
+++ 08-ng-model/todo-input.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {TodoService} from './todo-service';
+
+@Component({
+	selector: 'todo-input',
+	template: `<div>
+	<form (submit)="onSubmit()">
+	<input type="text" [(ngModel)]="todoModel">
+	</form>
+	</div>`
+})
+export class TodoInput {
+	todoModel;
+	
+	constructor(public todoService: TodoService) {
+	}
+
+	onSubmit(){
+		this.todoService.todos.push(this.todoModel)
+		console.log(this.todoService.todos);
+	}
+}
